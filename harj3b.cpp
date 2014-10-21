9b4531316831474ed6866e7772ee8eaaf1e5136d
KasperiSa <crakizor@luukku.com>
2014-10-21 10:31:22 +0300

    harj3b
    näyttää tiedot tyyllikkäämmin

---
+++ harj3b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "**********************************\n";
+		cout << "* *\n";
+	cout << "* Derpi *\nOsoite esim korvatie 12 B 14";
+	cout << "* Testy *\n";
+	cout << "* *\n";
+		cout << "*-------------------------------------- *\n";
+		cout << "* *\n";
+		cout << "* korvatie 12 B 14\n";
+		cout << "* 40520 JKL *\n";
+		cout << "* *\n";
+		cout << "**********************************\n";
+}
